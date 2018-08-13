0c32709209d03ad7ba2cbbc077b1e4ec96c7bc37
Ryan Bratton <tarrickryan@gmail.com>
2018-08-13 14:50:58 -0500

    add some pop

--- docs/app.js
+++ docs/app.js
@@ -18,3 +18,49 @@ $('a.smooth-scroll').on('click', function(event) {
     }, 1500, 'easeInOutExpo');
     event.preventDefault();
 });
+
+
+
+
+$('#custom-bikes .row').one('inview', function(){
+    console.log('inview' + $(this).data('class'));
+    $$('#numberCounter').css('background', 'orange');
+})
+
+$('.body #contact').one('inview', function(){
+    console.log('inview' + $(this).data('class'));
+    $('#numberCounter').style.backgroundColor = "pink";
+})
+
+
+
+
+$('#numberCounter').one('inview', function(event, isInView) {
+  if (isInView) {
+
+      $('#numberCounter').css('background', 'orange');
+      } else {
+        $('#numberCounter').style.backgroundColor = "pink";
+      }
+    });
+
+
+    // Animate the element's value from x to y:
+      $({someValue: 0}).animate({someValue: 450000}, {
+          duration: 2500,
+          easing:'swing', // can be anything
+          step: function() { // called on every step
+              // Update the element's text with rounded-up value:
+              $('.number').text(commaSeparateNumber(Math.round(this.someValue)));
+          },
+          complete:function(){
+              $('.number').text(commaSeparateNumber(Math.round(this.someValue)));
+          }
+      });
+
+     function commaSeparateNumber(val){
+        while (/(\d+)(\d{3})/.test(val.toString())){
+          val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        }
+        return val;
+      }
